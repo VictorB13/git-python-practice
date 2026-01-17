8e19260e6ad6745af06247896984b2b5c322aa69
Victor Butkov <butkovvictor@gmail.com>
2026-01-17 16:37:39 +0200

    Revert "added '!' chars to the return message"
    This reverts commit 31132daeab9c198247cc4a5ae078ca2ce39eac90.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import datetime
 def greet(name):
     now = datetime.datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    return f"Hello everyone, {name}! The current time is {current_time}!!!!!!!!!!!!!!"
+    return f"Hello everyone, {name}! The current time is {current_time}"
 
 if __name__ == "__main__":
     user = "World"
