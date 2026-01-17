2806742904a919943db77eda2d17f6075f8f48cb
Victor Butkov <butkovvictor@gmail.com>
2026-01-17 15:45:51 +0200

    app.py/return message was edited

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import datetime
 def greet(name):
     now = datetime.datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    return f"Hello, {name}! The current time is {current_time}"
+    return f"Hello my friends, {name}! The current time is {current_time}"
 
 if __name__ == "__main__":
     user = "World"
