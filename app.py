3ee8d6511079ac329c7f00f5412b8f12f51d038d
Victor Butkov <butkovvictor@gmail.com>
2026-01-17 15:38:57 +0200

    app.py/added time feature

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-def greet(name):
-    return f"Hello, {name}!"
+import datetime
 
+def greet(name):
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return f"Hello, {name}! The current time is {current_time}"
 
 if __name__ == "__main__":
     user = "World"
